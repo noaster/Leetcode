33e4875faa0370eac22ea9e7f95c4ab34fc37f9b
Chloe Yang <31306182+noaster@users.noreply.github.com>
2017-11-19 03:07:46 +0900

    add twosum.cpp

---
+++ TwoSum/twosum.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> results;
+        
+        int vecSize = nums.size();
+        for (int i = 0; i < vecSize; ++i)
+        {
+            for (int j = i + 1; j < vecSize; ++j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    results.push_back(i);
+                    results.push_back(j);
+                }
+            }
+        }
+
+        return results;
+    }
+};
