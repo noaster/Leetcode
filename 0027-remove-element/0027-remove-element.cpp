948b9cd830837f57a044c7bfef176c109570321e
Chloe Yang <31306182+noaster@users.noreply.github.com>
2024-06-01 19:53:36 +0900

    Time: 6 ms (27.75%), Space: 10.4 MB (81.16%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int size = nums.size();
+        if (size <= 0) {
+          return 0;
+        }
+        int tmp;
+        int endIndex = size - 1;
+        for (int i = 0; i <= endIndex; ++i) {
+            if (nums[i] == val) {
+                while (endIndex > i && nums[endIndex] == val) {
+                    endIndex--;
+                }
+                if (endIndex <= i) {
+                    break;
+                }
+                cout << i << ',' << endIndex << endl;
+                tmp = nums[i];
+                nums[i] = nums[endIndex];
+                nums[endIndex] = tmp;
+            }
+        }
+        if (nums[endIndex] != val) {
+          endIndex++;
+        }
+        return endIndex;
+    }
+};
